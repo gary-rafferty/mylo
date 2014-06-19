567b3a028b05b76d5ef951a09b1fd1bb775f5a88
Gary Rafferty <gary.rafferty@gmail.com>
2014-06-19 21:56:32 +0100

    Add object relationships and structures

--- models.rb
+++ models.rb
@@ -10,8 +10,49 @@ class User
   field :user_id, type: Integer
   field :email, type: String
   field :token, type: String
+  field :address, type: String
+
+  has_many :payees
+  has_many :subscriptions
 
   index({user_id: 1}, {unique: true, name: 'user_id_index'})
+end
+
+class Payee
+  include Mongoid::Document
+  include Mongoid::Timestamps
+
+  field :name, type: String
+  field :address, type: String
+
+  belongs_to :user
+
+  has_many :subscriptions
+end
+
+class Subscription
+  include Mongoid::Document
+  include Mongoid::Timestamps
+
+  field :amount, type: Integer
+  field :description, type: String
+  field :recurrence, type: String
+
+  belongs_to :payee
+  belongs_to :user
+end
+
+class Payment
+  include Mongoid::Document
+  include Mongoid::Timestamps
+
+  field :amount, type: Integer
+  field :address, type: String
+  field :type, type: String
+
+  validates_inclusion_of :type, in: ['Subscription', 'Oneoff']
+
+  belongs_to :user
 end
 
 class ReddcoinAddress
